ddba37a2389da739464e62ee3a79ca66b1733bec
Magnus Alexander Hagmar <m.hagmar@student.unsw.edu.au>
2016-03-05 13:33:16 +1100

    Expanded skeleton

--- ass1/adaptiveHuffman.cpp
+++ ass1/adaptiveHuffman.cpp
@@ -5,21 +5,31 @@ class AdaptiveHuffmanModel {
     class Node {
         public:
             int weight;
+            char symbol;
             Node* lchild;
             Node* rchild;
 
             Node();
             Node(int);
+            ~Node();
     };
     private:
         Node* root;
     public:
         AdaptiveHuffmanModel();
+        ~AdaptiveHuffmanModel();
+
+        std::string encode(char);
 };
 
 AdaptiveHuffmanModel::AdaptiveHuffmanModel() {
-    Node n = Node();
-    root = &n;
+    root = new Node;
+    std::cout << "Model created" << std::endl;
+}
+
+AdaptiveHuffmanModel::~AdaptiveHuffmanModel() {
+    delete root;
+    std::cout << "Model destroyed" << std::endl;
 }
 
 AdaptiveHuffmanModel::Node::Node() {
@@ -30,5 +40,21 @@ AdaptiveHuffmanModel::Node::Node(int w) {
     weight = w;
     lchild = NULL;
     rchild = NULL;
+    std::cout << "Node created" << std::endl;
 }
 
+AdaptiveHuffmanModel::Node::~Node(){
+    if (lchild) {
+        delete lchild;
+    }
+    if (rchild) {
+        delete rchild;
+    }
+    std::cout << "Node destroyed" << std::endl;
+}
+
+int main(int argc, char** argv){
+    AdaptiveHuffmanModel* a = new AdaptiveHuffmanModel;
+    std::cout << "tjenna" << std::endl;
+    return 0;
+}

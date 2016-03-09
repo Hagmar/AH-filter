4e64a054a9b648ec1e349d68801117fc8fdcbb4d
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-09 13:23:56 +1100

    Moved class declaration to header file

--- ass1/adaptiveHuffman.cpp
+++ ass1/adaptiveHuffman.cpp
@@ -1,36 +1,6 @@
 #include <iostream>
 #include "adaptiveHuffman.h"
 
-class AdaptiveHuffmanModel {
-    class Node {
-        public:
-            int weight;
-            char symbol;
-            unsigned char number;
-            Node* parent;
-            Node* lchild;
-            Node* rchild;
-
-            Node();
-            Node(int);
-            ~Node();
-    };
-    private:
-        Node* root;
-        Node* nyt;
-    public:
-        AdaptiveHuffmanModel();
-        ~AdaptiveHuffmanModel();
-
-        bool newSymbol(char);
-        Node* splitNYT();
-        Node* findNode(char);
-        Node* findMaxInBlock(int);
-        Node* findMaxInBlockRecursive(int, Node*, int);
-        void switchNodes(Node*, Node*);
-        std::string encode(char);
-};
-
 AdaptiveHuffmanModel::AdaptiveHuffmanModel() {
     root = new Node;
     nyt = root;
--- ass1/adaptiveHuffman.h
+++ ass1/adaptiveHuffman.h
@@ -1,6 +1,34 @@
 #ifndef ADAPTIVEHUFFMAN_H
 #define ADAPTIVEHUFFMAN_H
 
-class AdaptiveHuffmanModel;
+class AdaptiveHuffmanModel {
+    class Node {
+        public:
+            int weight;
+            char symbol;
+            unsigned char number;
+            Node* parent;
+            Node* lchild;
+            Node* rchild;
+
+            Node();
+            Node(int);
+            ~Node();
+    };
+    private:
+        Node* root;
+        Node* nyt;
+    public:
+        AdaptiveHuffmanModel();
+        ~AdaptiveHuffmanModel();
+
+        bool newSymbol(char);
+        Node* splitNYT();
+        Node* findNode(char);
+        Node* findMaxInBlock(int);
+        Node* findMaxInBlockRecursive(int, Node*, int);
+        void switchNodes(Node*, Node*);
+        std::string encode(char);
+};
 
 #endif

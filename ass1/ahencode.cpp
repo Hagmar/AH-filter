792ee023d767219d2d3d15ec734ab01bd17c9670
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-09 13:42:50 +1100

    Changed AHEncoder to a derived class of the abstract filter

--- ass1/ahencode.cpp
+++ ass1/ahencode.cpp
@@ -2,14 +2,11 @@
 #include "filter.h"
 #include "ahencode.h"
 
-int returnCode = 0;
-
-void process(std::string line){
+void AHEncodeFilter::process(std::string line){
     std::cout << line << std::endl;
 }
 
 int main( int argc, char** argv ) {
-	filter(argc, argv, &process);
 	std::cout.flush();
-	return std::cout ? returnCode : 2;
+    return 0;
 }
--- ass1/ahencode.h
+++ ass1/ahencode.h
@@ -1,7 +1,15 @@
 #ifndef AHENCODE_H
 #define AHENCODE_H
 
-void process(std::string line);
+#include "filter.h"
+
+class AHEncodeFilter: public Filter {
+    private:
+        bool split;
+
+        void process(std::string);
+};
+
 int main(int argc, char** argv);
 
 #endif

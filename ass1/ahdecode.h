be3c27d2b43dbc0e7e10fab55f00892e37c1399a
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-09 13:59:12 +1100

    Converted ahdecode to a derived class

--- ass1/ahdecode.h
+++ ass1/ahdecode.h
@@ -1,7 +1,15 @@
 #ifndef AHDECODE_H
 #define AHDECODE_H
 
-void process(std::string line);
+#include "filter.h"
+
+class AHDecodeFilter: public Filter {
+    private:
+        bool split;
+
+        void process(std::string);
+};
+
 int main(int argc, char** argv);
 
 #endif

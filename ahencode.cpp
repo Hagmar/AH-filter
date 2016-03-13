6942d4ba7d2ab68df2afe419f900037ed3b9bcc3
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-13 14:27:49 +1100

    Now correctly encodes an ASCII string

--- ahencode.cpp
+++ ahencode.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "filter.h"
 #include "ahencode.h"
+#include "adaptiveHuffman.h"
 
 void AHEncodeFilter::process(std::string line){
-    std::cout << line << std::endl;
+    AdaptiveHuffmanModel* model = new AdaptiveHuffmanModel;
+
+    std::string::iterator it = line.begin();
+    for (; it != line.end(); ++it){
+        std::cout << model->encode(*it) << std::endl;
+        model->updateModel(*it);
+    }
 }
 
 int main( int argc, char** argv ) {

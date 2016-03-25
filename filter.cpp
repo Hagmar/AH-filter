9ebf27d263e8b18f6e5ecbddb918c909c379f276
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-25 15:57:59 +1100

    Comments documenting filter source files

--- filter.cpp
+++ filter.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include "filter.h"
 
+// Iterates through the lines in an input stream, processing them in whatever
+// way the class instance's process function is defined
 void Filter::processStream(std::istream& stream){
     std::string line;
     while (getline(stream, line)){
@@ -9,6 +11,7 @@ void Filter::processStream(std::istream& stream){
     }
 }
 
+// Attempts to open an input stream for stdin or the specified files
 void Filter::processFile(std::string const& filename) {
     if (filename == "-") {
         processStream(std::cin);
@@ -22,6 +25,8 @@ void Filter::processFile(std::string const& filename) {
     }
 }
 
+// Determines whether to process stdin or a list of files supplied on the
+// command line
 void Filter::filter(int argc, char** argv) {
     if (argc == 1) {
         processFile("-");
--- filter.h
+++ filter.h
@@ -1,6 +1,9 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+// Abstract class intended to create UNIX-like filters
+// The virtual member function process is intended to be defined in
+// any derived classes
 class Filter {
     protected:
         void processFile(std::string const&);

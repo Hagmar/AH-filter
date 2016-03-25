379607a39a29d512d2b2b609bab64a1f0b83d42d
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-03-25 17:40:02 +1100

    Comments documenting decoder

--- ahdecode.cpp
+++ ahdecode.cpp
@@ -11,6 +11,7 @@ AHDecodeFilter::AHDecodeFilter(bool s) {
     split = s;
 }
 
+// Overrides the filter function to add support for the "-s" option
 void AHDecodeFilter::filter(int argc, char** argv) {
     if (argc <= 1 || (split && argc == 2)) {
         processFile("-");
@@ -24,12 +25,18 @@ void AHDecodeFilter::filter(int argc, char** argv) {
     std::cout.flush();
 }
 
+// The decoding filter's way of processing lines of input
+// It sends an entire line of input to the model for decoding since it cannot
+// know how many bits the next character is comprised of
 void AHDecodeFilter::process(std::string line){
     AdaptiveHuffmanModel* model = new AdaptiveHuffmanModel(split);
     std::cout << model->decode(line) << std::endl;
     delete model;
 }
 
+// Determine whether the input characters are split into groups by spaces
+// In practise, the -s makes no difference, as the decoder ignores all
+// characters other than 1 and 0
 int main( int argc, char** argv ) {
     bool split = false;
     if (argc > 1){
